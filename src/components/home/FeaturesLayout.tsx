80a8a066946c82ced527e92d5d6c9a64d57c49f0
SAJID <sajid.munir4428@hotmail.com>
2024-04-14 12:21:39 +0500

    styling updates

--- src/components/home/FeaturesLayout.tsx
+++ src/components/home/FeaturesLayout.tsx
@@ -1,18 +1,21 @@
+import { useState } from "react";
 import LayoutHeader from "../shared/LayoutHeader";
 import FeatureDetailCard from "./FeatureDetailCard";
 import styles from "./FeaturesLayout.module.css";
 
 function FeaturesLayout()
 {
+    const [buttonIndex,setButtonIndex] = useState(0);
+
     return (<div className={styles.container}>
         <LayoutHeader headingTextWhite="Our " headingTextColored="Features" description="Experience a host of powerful features 
         at YourBank, including seamless online banking, secure transactions, 
         and personalized financial insights, all designed to enhance your banking experience"/>
         <div className={styles.contentContainer}>
             <div className={styles.buttonSection}>
-                <p>Online Banking</p>
-                <p>Financial Tools</p>
-                <p>Customer Support</p>
+                <button className={styles.button}>Online Banking</button>
+                <button className={styles.button}>Financial Tools</button>
+                <button className={styles.button}>Customer Support</button>
             </div>
             <div className={styles.featureDetailSection}>
                 <FeatureDetailCard heading="24/7 Account Access" description="Enjoy the convenience of accessing your accounts anytime, anywhere through our secure online 
--- src/components/home/UseCaseCard.tsx
+++ src/components/home/UseCaseCard.tsx
@@ -1,4 +1,4 @@
-import LinkButton from "../shared/LinkButton";
+import GradientIconBackground from "../shared/GradientIconBackground";
 import styles from './UseCaseCard.module.css';
 
 interface Props{
@@ -24,30 +24,14 @@ function UseCaseCard(props : Props)
 {
     return (<div className={styles.container}>
         <div className={`${styles.contentSection} ${props.reverseOrder && styles.lastOrder}`}>
-            <div className={styles.infoPoint}>
+            {props.infoPoints.map((point) => (
+                <div className={styles.infoPoint}>
                 <div>
-                    <img src={props.infoPoints[0].imageLink}/>
+                    <GradientIconBackground imageLink={point.imageLink}/>
                 </div>
-                <p>{props.infoPoints[0].headingText}</p>
-            </div>
-            <div className={styles.infoPoint}>
-                <div>
-                    <img src={props.infoPoints[1].imageLink}/>
+                <p>{point.headingText}</p>
                 </div>
-                <p>{props.infoPoints[1].headingText}</p>
-            </div>
-            <div className={styles.infoPoint}>
-                <div>
-                    <img src={props.infoPoints[2].imageLink}/>
-                </div>
-                <p>{props.infoPoints[2].headingText}</p>
-            </div>
-            <div className={styles.infoPoint}>
-                <div>
-                    <img src={props.infoPoints[3].imageLink}/>
-                </div>
-                <p>{props.infoPoints[3].headingText}</p>
-            </div>
+            ))}
         </div>
         <div className={`${styles.textSection} ${props.reverseOrder && styles.firstOrder}`}>
             <div>
@@ -55,21 +39,13 @@ function UseCaseCard(props : Props)
                 <p className={styles.descriptionText}>{props.description}</p>
             </div>
             <div className={styles.statsSection}>
-                <div>
-                    <p className={styles.figuresText}>{props.statPoints[0].figure}%</p>
-                    <p className={styles.figureInfoText}>{props.statPoints[0].text}</p>
-                </div>
-                <div>
-                    <p className={styles.figuresText}>{props.statPoints[1].figure}%</p>
-                    <p className={styles.figureInfoText}>{props.statPoints[1].text}</p>
-                </div>
-                <div>
-                    <p className={styles.figuresText}>{props.statPoints[2].figure}%</p>
-                    <p className={styles.figureInfoText}>{props.statPoints[2].text}</p>
-                </div>
+                {props.statPoints.map((point) => (<div>
+                    <p className={styles.figuresText}>{point.figure}%</p>
+                    <p className={styles.figureInfoText}>{point.text}</p>
+                </div>))}
             </div>
-            <div>
-                <LinkButton buttonText="Learn More" link="#"/>
+            <div className={styles.buttonContainer}>
+                <a className={styles.linkButton} href="#">Learn More</a>
             </div>
         </div>
     </div>);

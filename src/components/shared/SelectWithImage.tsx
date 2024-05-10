4bd573727d78797da5e1d339d130f6f3c7c895c0
SAJID <sajid.munir4428@hotmail.com>
2024-05-10 17:38:49 +0500

    Update SelectWithImage.tsx

--- src/components/shared/SelectWithImage.tsx
+++ src/components/shared/SelectWithImage.tsx
@@ -1,36 +1,6 @@
-import { useState } from 'react';
-import Select, { ActionMeta } from 'react-select';
 
-export interface CustomOption {
-    value: string,
-    imageLink: string,
-    label: string
-}
-
-interface Props{
-    options : CustomOption[]
-}
-
-const options: CustomOption[] = [
-  { value: '1', label: 'Option 1', imageLink: 'path_to_image_1' },
-  { value: '2', label: 'Option 2', imageLink: 'path_to_image_2' },
-  { value: '3', label: 'Option 3', imageLink: 'path_to_image_3' },
-];
-
-function SelectWithImage(props : Props) {
-    const [selectedOption, setSelectedOption] = useState(options[0]);
-
-    const onChange = (option: CustomOption | null, actionMeta: ActionMeta<CustomOption>) => {
-        setSelectedOption(selectedOption);
-     }
-
-    return (<div className="App">
-            <Select
-            defaultValue={selectedOption}
-            onChange={onChange}
-            options={options}
-            />
-            </div>);
+function SelectWithImage() {
+    return (<></>);
 }
 
 export default SelectWithImage;
